7ba340c4e4f9297dd682f8f09a34d0d80976e485
majunjie_yx@163.com <45347041+xiaomage89@users.noreply.github.com>
2023-04-13 09:25:14 +0800

    前端Vue 初始架构

--- hn-stock-web/src/main.js
+++ hn-stock-web/src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en'
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
